0f7e3076c31b9b295ef04c508f9a817853b3b0ec
IBBU93 <iibrahim9626@gmail.com>
2023-12-18 21:05:50 +0530

    first commit

---
+++ CRUD/crud/src/Component/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main> 
+      
+      <h5>12 Unique UI/UX Project Ideas to Boost Your Portfolio
+
+‍as a UX designer, building a strong portfolio is crucial to landing your dream job</h5>
+    </main>
+      
+    
+  )
+}
+
+export default Home
